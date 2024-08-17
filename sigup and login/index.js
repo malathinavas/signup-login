0c004c5f00e5eb5090d6aca193a87afcc6cdc030
malathinavas <160415506+malathinavas@users.noreply.github.com>
2024-08-17 19:08:51 +0530

    Add files via upload

---
+++ sigup and login/index.js
@@ -0,0 +1,16 @@
+// Define an object representing a person
+const person = {
+    username: 'malathi',
+    email: 'mathinm98@gmail.com',
+    password: 1407,
+    
+    greet: function() {
+      console.log(`Hello, my name is ${this.firstName} ${this.lastName}.`);
+    }
+  };
+  
+  console.log(person.firstName);
+  console.log(person.age); 
+  
+  
+  
